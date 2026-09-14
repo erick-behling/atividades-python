fd50ec757f759a3bdbd592b9d0761da16d6d3a08
erick-behling <erick_b_madeira@estudante.sesienai.org.br>
2026-09-14 14:53:11 -0300

    add - atividade 2

---
+++ exercicio_2.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}! Tenha um bom dia!")
+
+# input é o comando para lermos um dado (ele sempre lê como string [texto])
+# print é o comando para exibirmos uma mensagem
+# f (f-string) é um argumento para formatar conteúdo de variável em texto
