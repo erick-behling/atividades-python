adcb1f3e01a6a22a8f1afdee2cd844a5a85a14e4
erick-behling <erick_b_madeira@estudante.sesienai.org.br>
2026-09-14 15:00:18 -0300

    add - atividade 8

---
+++ exercicio_8.py
@@ -0,0 +1,14 @@
+# jogo = "Mario"
+# print(f"Conteúdo da variável: {jogo}")
+
+# jogo = "Zelda"
+# print(f"Conteúdo da variável: {jogo}")
+
+jogos = []
+print(f"Conteúdo atual: {jogos}")
+
+jogos.append("The Last of Us")
+jogos.append("Counter-Strike 2")
+jogos.append("Dragon Ball Z: Kakarot")
+print(f"Conteúdo atual: {jogos}")
+print(f"Conteúdo: {jogos[1]}")
